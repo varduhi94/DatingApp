8ff16eed754e40ae2e064b9aae6dd0a4c8144c50
Varduhi Grigoryan <grigoryanvarduhi94@gmail.com>
2020-02-27 13:33:11 +0100

    End Of section 8

---
+++ DatingApp2.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp2.API.Dtos;
+using DatingApp2.API.Models;
+
+namespace DatingApp2.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForDetailsDto>()
+                .ForMember(dest => dest.PhotoUrl, opt 
+                    => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt 
+                    => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt 
+                    => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt 
+                    => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoForDetailsDto>();
+        }
+    }
+}
--- DatingApp2.API/Startup.cs
+++ DatingApp2.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp2.API.Data;
 using DatingApp2.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -20,6 +21,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
+
 namespace DatingApp2.API
 {
     public class Startup
@@ -35,9 +37,15 @@ namespace DatingApp2.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = 
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters 
